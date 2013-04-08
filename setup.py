f5ee54f150a3f6a64eedeae91b4383092f4a886d
Josh Mize <jgmize@gmail.com>
2013-04-08 00:44:27 -0500

    version 0.8.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='django-urlographer',
-    version='0.8.1',
+    version='0.8.2',
     author='Josh Mize',
     author_email='jmize@consumeraffairs.com',
     description='URL mapper for django',
