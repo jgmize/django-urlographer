2a6f360f1e662a570c4634bbf0afb51f718c8b7b
Jeremy Satterfield <jsatt22@gmail.com>
2013-05-21 14:37:57 -0500

    version bump to 0.8.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name='django-urlographer',
-    version='0.8.7',
+    version='0.8.8',
     author='Josh Mize',
     author_email='jmize@consumeraffairs.com',
     description='URL mapper for django',
