2890e9f98619b736659aae5f9cbe6180660103fd
Josh Mize <jgmize@gmail.com>
2013-07-24 16:26:57 +0000

    remove broken django_jenkins integration, add ipdbplugin

--- test_app/settings.py
+++ test_app/settings.py
@@ -22,22 +22,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_nose',
     'south',
+    'django_nose',  # must come after south to override south's test command
     'urlographer',
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = ('--nocapture', )
-
-import sys
-if 'jenkins' in sys.argv:
-    INSTALLED_APPS += ('django_jenkins',)
-    PROJECT_APPS = ('urlographer',)
-    COVERAGE_RCFILE = 'test_app/coveragerc'
-    JENKINS_TASKS = (
-        'django_jenkins.tasks.with_coverage',
-        'django_jenkins.tasks.django_tests',
-        'django_jenkins.tasks.run_pep8',
-        'django_jenkins.tasks.run_pyflakes',
-    )
